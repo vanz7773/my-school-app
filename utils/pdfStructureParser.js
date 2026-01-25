a0c78ff8b7a505aa33ae629ad5f4aa08ce7d6da1
vanz7773 <oseievanslolo@gmail.com>
2026-01-25 15:39:45 +0000

    parser 2.0

--- utils/pdfStructureParser.js
+++ utils/pdfStructureParser.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
-const pdf = require("pdf-parse");
+const pdfParse = require("pdf-parse");
+
 
 /**
  * STEP 1: Extract raw text from PDF
  */
 async function extractPdfText(pdfPath) {
   const buffer = fs.readFileSync(pdfPath);
-  const data = await pdf(buffer);
+  const data = await pdfParse(buffer);
   return data.text || "";
 }
 
