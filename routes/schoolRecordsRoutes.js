9c4ca80e70be95b6bffd2d17006aecb11c57377c
vanz7773 <oseievanslolo@gmail.com>
2026-02-17 20:44:50 +0000

    refactor: rename authorization middleware from `authorize` to `restrictTo`

--- routes/schoolRecordsRoutes.js
+++ routes/schoolRecordsRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const schoolRecordsController = require('../controllers/schoolRecordsController');
-const { protect, authorize } = require('../middlewares/authMiddleware');
+const { protect, restrictTo } = require('../middlewares/authMiddleware');
 
 // Route to get all records for the school
-router.get('/', protect, authorize('admin'), schoolRecordsController.getSchoolRecords);
+router.get('/', protect, restrictTo('admin'), schoolRecordsController.getSchoolRecords);
 
 // Route to update (or create) a record for a specific class
-router.put('/', protect, authorize('admin'), schoolRecordsController.updateSchoolRecord);
+router.put('/', protect, restrictTo('admin'), schoolRecordsController.updateSchoolRecord);
 
 module.exports = router;
