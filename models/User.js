df9c8fd17b065f1ff26b1a2129d2a882f572130e
vanz7773 <oseievanslolo@gmail.com>
2026-02-26 15:11:51 +0000

    parent

--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema(
 
     gender: {
       type: String,
-      enum: ["Male", "Female"],
+      enum: ["Male", "Female", null, ""],
       default: null,
     },
 
