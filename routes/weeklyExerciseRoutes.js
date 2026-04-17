290d0246866d8cf0a99fe7ef0f90e53e243c9b6c
vanz7773 <oseievanslolo@gmail.com>
2026-04-17 02:20:22 +0000

    feat: add PATCH route to update individual weekly exercises

--- routes/weeklyExerciseRoutes.js
+++ routes/weeklyExerciseRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { protect, restrictTo } = require('../middlewares/authMiddleware');
-const { addOrUpdateExercise, getExerciseSummary, finalizeWeek, getExercisesForStudent } = require('../controllers/weeklyExerciseController');
+const { addOrUpdateExercise, updateExercise, getExerciseSummary, finalizeWeek, getExercisesForStudent } = require('../controllers/weeklyExerciseController');
 
 // Teacher adds or updates exercise count
 router.post('/', protect, restrictTo('admin', 'teacher'), addOrUpdateExercise);
+router.patch('/:exerciseId', protect, restrictTo('teacher'), updateExercise);
 router.post('/finalize', protect, restrictTo('teacher'), finalizeWeek);
 
 // Admin/Teacher views summary
